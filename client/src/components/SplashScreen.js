f43ec234140827eb317461e5b9e6f7c32ab2ad66
Eddie Zhu <hongtai.zhu@stonybrook.edu>
2021-12-06 02:30:48 -0500

    Added all lists and community lists screen
    also added buttons to welcome screen

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -1,12 +1,16 @@
 import Button from '@mui/material/Button';
+import { Typography } from '@mui/material';
+import { Link } from 'react-router-dom'
 export default function SplashScreen() {
     return (
         <div id="splash-screen">
             Welcome to <br />The Top 5
             Lister
             <br />
-            Eddie Zhu
-            
+            <Button variant="outlined"><Link to='/login/'>Login</Link></Button>
+            <Button variant="outlined"><Link to='/register/'>Create New Account</Link></Button>
+            <Button variant="outlined"><Link to='/register/'>Login As Guest</Link></Button>
+            <Typography>Eddie Zhu</Typography>
             
         </div>
         
